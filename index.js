13e4d2d432ee1cc9ef376adffc7ca723d799227f
Steeve Henrard <56322140+Steeve1301@users.noreply.github.com>
2020-06-15 10:11:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,47 +1,57 @@
+let http = require('http');
+
+
 let io= require('socket.io')(process.env.PORT || 5000);
 
-//Custom Classes
-let Player= require('./Classes/Player.js');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write(req.url);
+  res.end();
+    
+    let Player= require('./Classes/Player.js');
 
-let players= [];
-let sockets= [];
+    let players= [];
+    let sockets= [];
 
-console.log('Server has started');
+    console.log('Server has started');
 
-io.on('connection', (socket)=>{
-    console.log("Connection made!");
+    io.on('connection', (socket)=>{
+        console.log("Connection made!");
 
-    let player= new Player();
-    let thisPlayerID = player.id;
+        let player= new Player();
+        let thisPlayerID = player.id;
 
-    players[thisPlayerID] = player;
-    sockets[thisPlayerID] = socket;
+        players[thisPlayerID] = player;
+        sockets[thisPlayerID] = socket;
 
-    //Tell the client that is our id for the server
-    socket.emit('register', {id: thisPlayerID});
-    socket.emit('spawn', player); //Tell myself i have spawned
-    socket.broadcast.emit('spawn', player); //Tell others i have spawned
+        //Tell the client that is our id for the server
+        socket.emit('register', {id: thisPlayerID});
+        socket.emit('spawn', player); //Tell myself i have spawned
+        socket.broadcast.emit('spawn', player); //Tell others i have spawned
 
 
-    //Tell myself about everyone else in the game
-    for(let playerID in players){
-        if(playerID != thisPlayerID){
-            socket.emit('spawn', player[playerID]);
+        //Tell myself about everyone else in the game
+        for(let playerID in players){
+            if(playerID != thisPlayerID){
+                socket.emit('spawn', player[playerID]);
+            }
         }
-    }
 
-    //Positional Data from Client
-    socket.on('updatePosition', (data)=>{
-        player.position.x = data.position.x;
-        player.position.y= data.position.y;
+        //Positional Data from Client
+        socket.on('updatePosition', (data)=>{
+            player.position.x = data.position.x;
+            player.position.y= data.position.y;
 
-        socket.broadcast.emit('updatePosition', player);
+            socket.broadcast.emit('updatePosition', player);
+        });
+
+        socket.on('disconnect', () =>{
+           console.log("A player is now disconnected");
+           delete players[thisPlayerID];
+           delete sockets[thisPlayerID];
+           socket.broadcast.emit('disconnected', player);
+        });
     });
 
-    socket.on('disconnect', () =>{
-       console.log("A player is now disconnected");
-       delete players[thisPlayerID];
-       delete sockets[thisPlayerID];
-       socket.broadcast.emit('disconnected', player);
-    });
-});
+}).listen(8000);
+//Custom Classes
